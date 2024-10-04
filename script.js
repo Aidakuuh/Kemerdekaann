4aa1ffe5eaa848762389cb5bd0c22328ff0cff82
Aidakuuh <aidakuuh@gmail.com>
2024-10-04 23:22:47 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+//change and icon (dark mode)
+
+const mode = document.querySelector('.mode');
+const icon = document.querySelector('.fa-moon');
+const Garda = document.querySelector('.Garda');
+
+mode.addEventListener('click', function(){
+    document.body.classList.toggle('dark-mode');
+
+    if(document.body.classList.contains('dark-mode')){
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+        Garda.classList.add('Garda-dark');
+    }
+    else{
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+        Garda.classList.remove('Garda-dark');
+    }
+
+})
+
+// change toggle-btn navbar
+const bars = document.querySelector ('.toggle-btn');
+const navbar = document.querySelector ('header .navbar');
+
+bars.addEventListener('click', function(){
+    if(bars.classList.contains('fa-bars')) {
+        bars.classList.remove('fa-bars');
+        bars.classList.add('fa-xmark');
+        navbar.classList.add('slide');
+    }
+    else {
+        bars.classList.remove('fa-xmark');
+        bars.classList.add('fa-bars');
+        navbar.classList.remove('slide');
+    }
+})
